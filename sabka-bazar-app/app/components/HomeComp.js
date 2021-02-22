00c540a3ceabdabde97343179eee13e094e1acb8
ansyadav <anshikay1993@gmail.com>
2021-02-22 20:40:24 +0530

    new file changes

--- sabka-bazar-app/app/components/HomeComp.js
+++ sabka-bazar-app/app/components/HomeComp.js
@@ -1,51 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
-import bannerimg1 from '../images/offers/offer1.jpg';
-import bannerimg2 from '../images/offers/offer2.jpg';
-import bannerimg3 from '../images/offers/offer3.jpg';
-import bannerimg4 from '../images/offers/offer4.jpg';
-import bannerimg5 from '../images/offers/offer5.jpg';
-import product1 from '../images/category/beverages.png';
-import product2 from '../images/category/bakery.png';
-import product3 from '../images/category/beauty.png';
-import product4 from '../images/category/baby.png';
-import product5 from '../images/category/fruits.png';
-
+import axios from 'axios';
 
 
 const HomeComp = () => {
+    const [bannersObject, setBannersObject] = useState([]);
+    const [productsObject, setProductsObject] = useState([]);
     const banners = [
         {
-            "bannerImageUrl": bannerimg1,
+            "bannerImageUrl": '../images/offers/offer1.jpg',
             "bannerImageAlt": "Independence Day Deal - 25% off on shampoo",
             "isActive": true,
             "order": 1,
             "id": "5b6c38156cb7d770b7010ccc"
         },
         {
-            "bannerImageUrl": bannerimg2,
+            "bannerImageUrl": '../images/offers/offer2.jpg',
             "bannerImageAlt": "Independence Day Deal - Rs120 off on surf",
             "isActive": true,
             "order": 2,
             "id": "5b6c38336cb7d770b7010ccd"
         },
         {
-            "bannerImageUrl": bannerimg3,
+            "bannerImageUrl": '../images/offers/offer3.jpg',
             "bannerImageAlt": "Independence Day Deal - Rs99 off on domex",
             "isActive": true,
             "order": 3,
             "id": "5b6c38456cb7d770b7010cce"
         },
         {
-            "bannerImageUrl": bannerimg4,
+            "bannerImageUrl": '../images/offers/offer4.jpg',
             "bannerImageAlt": "Independence Day Deal - Rs99 off on bodywash",
             "isActive": true,
             "order": 4,
             "id": "5b6c38576cb7d770b7010ccf"
         },
         {
-            "bannerImageUrl": bannerimg5,
+            "bannerImageUrl": '../images/offers/offer5.jpg',
             "bannerImageAlt": "Independence Day Deal - Rs70 off on tea",
             "isActive": true,
             "order": 5,
@@ -60,7 +52,7 @@ const HomeComp = () => {
             "description": "Our beverage department will ensure your fridge is always fully stocked. Shop for soda, juice, beer and more. ",
             "enabled": true,
             "order": 3,
-            "imageUrl": product1,
+            "imageUrl": "../images/category/beverages.png",
             "id": "5b675e5e5936635728f9fc30"
         },
         {
@@ -69,7 +61,7 @@ const HomeComp = () => {
             "description": "The best cupcakes, cookies, cakes, pies, cheesecakes, fresh bread, biscotti, muffins, bagels, fresh coffee, milk and more.",
             "enabled": true,
             "order": 2,
-            "imageUrl": product2,
+            "imageUrl": "../images/category/bakery.png",
             "id": "5b6899123d1a866534f516de"
         },
         {
@@ -78,7 +70,7 @@ const HomeComp = () => {
             "description": "Buy beauty and personal care products online in India at best prices.",
             "enabled": true,
             "order": 4,
-            "imageUrl": product3,
+            "imageUrl": "../images/category/beauty.png",
             "id": "5b68994e3d1a866534f516df"
         },
         {
@@ -87,7 +79,7 @@ const HomeComp = () => {
             "description": "Shop online for Baby Products, Diapers, Skin Care Products,etc.",
             "enabled": true,
             "order": 5,
-            "imageUrl": product4,
+            "imageUrl": "../images/category/baby.png",
             "id": "5b6899683d1a866534f516e0"
         },
         {
@@ -104,18 +96,46 @@ const HomeComp = () => {
             "description": "A variety of fresh fruits and vegetables.",
             "enabled": true,
             "order": 1,
-            "imageUrl": product5,
+            "imageUrl": "../images/category/fruits.png",
             "id": "5b6899953d1a866534f516e2"
         }
     ]
+
+    /**
+     * Similar to componentDidMount and componentDidUpdate:
+     */
+    useEffect(() => {
+        setBannersObject(
+            ...bannersObject,
+            banners
+        )
+        setProductsObject(
+            ...productsObject,
+            products
+        )
+    }, []);
+
+    /**
+     * Code for load Dyanamic Image Urls without importing each image files
+     */
+    function importAll(r) {
+        let images = {};
+        r.keys().map((item, index) => { images[item.replace('./', '../images/')] = r(item); });
+        return images;
+    }
+    /**
+     * Use Importall and webpack Require.context('directory', useSubdirectories: boolean, regExp)
+     */
+    const images = importAll(require.context('../images/', true, /\.(png|jpe?g|svg)$/));
+
     return (
         <div className="homeWrap">
             <section className="slider-section">
                 <Carousel autoPlay>
-                    {banners.map(item => {
+                    {bannersObject.map(item => {
                         return (
                             <div key={item.order}>
-                                <img src={item.bannerImageUrl} alt={item.bannerImageAlt} />
+                                <img src={images[item.bannerImageUrl]} alt={item.bannerImageAlt} />
                             </div>
                         )
                     })}
@@ -124,10 +144,10 @@ const HomeComp = () => {
             <section className="product-info-list">
                 <ul>
                     {
-                        products.map(item => item.enabled && <li key={item.key}>
+                        productsObject.map(item => item.enabled && <li key={item.key}>
                             <div className="floatcontainer">
                                 <div className="product-image">
-                                    <img src={item.imageUrl} alt={item.name} />
+                                    <img src={images[item.imageUrl]} alt={item.name} />
                                 </div>
                                 <div className="product-info">
                                     <h2>{item.name}</h2>
